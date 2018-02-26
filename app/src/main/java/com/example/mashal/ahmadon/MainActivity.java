83290e308fb8f9c5b2246e3308633552352a02d4
ahmadm2001 <mashalm2001@gmail.com>
2018-02-26 10:40:49 +0200

    2nd commit

--- app/src/main/java/com/example/mashal/ahmadon/MainActivity.java
+++ app/src/main/java/com/example/mashal/ahmadon/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         x=(EditText) findViewById(R.id.editText4);
         e=(EditText) findViewById(R.id.editText5);
         r=(EditText) findViewById(R.id.editText6);
